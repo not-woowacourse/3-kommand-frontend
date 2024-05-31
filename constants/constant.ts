01b0ee75388b840878eca2d02876fb50ba5b4467
Joo Chanhwi <158996540+30kb@users.noreply.github.com>
2024-05-31 19:53:07 +0900

    chore: environment variables

---
+++ constants/constant.ts
@@ -0,0 +1,6 @@
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000';
+export const CLIENT_NAME =
+  process.env.NEXT_PUBLIC_CLIENT_NAME ?? 'example-user';
+
+export const DEFAULT_PATHNAME = process.env.NEXT_PUBLIC_DEFAULT_PATHNAME ?? '/';
