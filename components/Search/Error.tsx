7572d4c6f323e403c72af6689d186675b99489c8
Joo Chanhwi <158996540+30kb@users.noreply.github.com>
2024-06-06 21:41:56 +0900

    fix: fix color

--- components/Search/Error.tsx
+++ components/Search/Error.tsx
@@ -7,7 +7,7 @@ interface ErrorProps {
 
 export function Error({ icon: Icon, text }: ErrorProps) {
   return (
-    <div className="flex flex-col items-center gap-2 text-balance p-8 text-center text-sm font-medium text-base-500 dark:text-base-500">
+    <div className="flex flex-col items-center gap-2 text-balance p-8 text-center text-sm font-medium text-base-500 dark:text-base-dark-500">
       <Icon size={36} />
       <p>{text}</p>
     </div>
